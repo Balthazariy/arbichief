29e3c156ee7e79d56a93118e7e3edd5de137c0c1
Roman Malitskyi <chebureck77@gmail.com>
2026-02-10 20:43:25 +0200

    підтримка андроіду

---
+++ android/app/src/main/java/mk/ua/cheb/MainActivity.java
@@ -0,0 +1,5 @@
+package mk.ua.cheb;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mk.ua.cheb',
+  appName: 'arbichief',
+  webDir: 'dist'
+};
+
+export default config;
